be75bcc297d3f4e1789004ed51dce4454f825d9e
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-06 10:07:56 -0700

    hamburger menu created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className = 'App'>
+      <svg viewBox = '0 0 12 10' className = 'hamburger' height = '100px' width = '120px'>
+        <path d = 'M10,2 L2,2' className = 'upper' style = {{fill: 'none', stroke: '#474747', strokeLinecap: 'round'}}/>
+        <path d = 'M2,5 L10,5' className = 'middle' style = {{fill: 'none', stroke: '#474747', strokeLinecap: 'round'}}/>
+        <path d = 'M10,8 L2,8' className = 'lower' style = {{fill: 'none', stroke: '#474747', strokeLinecap: 'round'}}/>   
+    </svg>
     </div>
   );
 }
