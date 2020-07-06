7e43a64cbfa2ee9b13ad08538b7935c60d4c6a45
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-06 11:00:42 -0700

    got it to work! now for styling the menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import { Power2, TimelineLite } from 'gsap';
+import React, { useEffect, useRef } from 'react';
+import { Power2, TimelineLite, TweenMax } from 'gsap';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
 
-  var tl = new TimelineLite({paused: true, reversed: true});
+  const tl = new TimelineLite({paused: true, reversed: true});
+  let app = useRef(null);
+  let hamburger = useRef(null);
+  let upper = useRef(null);
+  let middle = useRef(null);
+  let lower = useRef(null);
 
-  tl
-    .to('.upper', 0.5, {attr: {d: 'M8,2 L2,8'}, x: 1, ease:Power2.easeInOut}, 'start')
-    .to('.middle', 0.5, {autoAlpha: 0}, 'start')
-    .to('.lower', 0.5, {attr: {d: 'M8,8 L2,2'}, x: 1, ease:Power2.easeInOut}, 'start');
+  useEffect(() => {
+    TweenMax.to(app, 0, {css: {visibility: 'visible'}})
+    tl
+    .to(upper, 0.5, {attr: {d: 'M8,2 L2,8'}, x: 1, ease:Power2.easeInOut}, 'start')
+    .to(middle, 0.5, {autoAlpha: 0}, 'start')
+    .to(lower, 0.5, {attr: {d: 'M8,8 L2,2'}, x: 1, ease:Power2.easeInOut}, 'start');
+    
+  }, [])
+
+  
     
     // document.querySelector('.hamburger').addEventListener('click', function(){
     //   tl.reversed() ? tl.play() : tl.reverse();
     // })
     function handleClick() {
-      console.log('clicked me');
+      tl.reversed() ? tl.play() : tl.reverse();
     }
 
   return (
-    <div className = 'App'>
+    <div className = 'App' ref = {el => app = el}>
       <svg 
       viewBox = '0 0 12 10' 
-      className = 'hamburger' 
+      className = 'hamburger'
+      ref = {el => hamburger = el} 
       height = '100px' 
       width = '120px'
       onClick = {handleClick}
@@ -31,6 +44,7 @@ function App() {
         <path 
         d = 'M10,2 L2,2' 
         className = 'upper' 
+        ref = {el => upper = el}
         style = {{
           fill: 'none', 
           stroke: '#474747', strokeLinecap: 'round'}}
@@ -38,6 +52,7 @@ function App() {
         <path 
         d = 'M2,5 L10,5' 
         className = 'middle' 
+        ref = {el => middle = el}
         style = {{
           fill: 'none', 
           stroke: '#474747', strokeLinecap: 'round'}}
@@ -45,6 +60,7 @@ function App() {
         <path 
         d = 'M10,8 L2,8' 
         className = 'lower' 
+        ref = {el => lower = el}
         style = {{
           fill: 'none', 
           stroke: '#474747', strokeLinecap: 'round'}}
